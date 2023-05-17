b9fd4b5d1e8774ad389c2af86a9f65a973624e87
Muneebahm1 <98212039+Muneebahm1@users.noreply.github.com>
2023-05-17 08:14:26 +0500

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    char buffer[10];
+    std::cout << "Enter a string: ";
+    std::cin >> buffer;
+    std::cout << "You entered: " << buffer << std::endl;
+    return 0;
+}
