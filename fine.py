ef0f94a7f0db256cbea9ddc375303d82090f1d1f
Muneebahm1 <98212039+Muneebahm1@users.noreply.github.com>
2023-05-17 08:06:56 +0500

    Create fine.py

---
+++ fine.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def main():
+    num1 = 5
+    num2 = 10
+    result = calculate_sum(num1, num2)
+    print("The sum is:", result)
+
+if __name__ == "__main__":
+    main()
